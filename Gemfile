819d106c4f5c328e50b76183febb951fd41987f3
hyosunko <hyosunko@gmail.com>
2019-03-23 20:55:38 -0700

    remove title from movie choices

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.5.3'
+ruby '>=2.5.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
@@ -66,5 +66,6 @@ gem "webpacker", "~> 3.5"
 gem "react-rails", "~> 2.4"
 
 gem "devise", "~> 4.6"
-# gem 'wdm', '>= 0.1.0'
 gem 'jquery-rails'
+# gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+# gem 'bcrypt', git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt'
--- app/javascript/components/pages/CardBuilder.js
+++ app/javascript/components/pages/CardBuilder.js
@@ -1,31 +1,43 @@
-import React, { Component } from 'react'
-import { Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
-import CardImage from './CardImage.js'
+import React, { Component } from "react";
+import {
+  Button,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import CardImage from "./CardImage.js";
 
 class CardBuilder extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.toggle = this.toggle.bind(this);
     this.state = {
       dropdownOpen: false,
-      selectedMovie: null,
-    }
+      selectedMovie: null
+    };
   }
-  cancelSelection = (e) => {
-    const { selectedMovie } = this.state
-    this.setState({selectedMovie:null});
-    this.props.cancelChoice(selectedMovie)
+  cancelSelection = e => {
+    const { selectedMovie } = this.state;
+    this.setState({ selectedMovie: null });
+    this.props.cancelChoice(selectedMovie);
     console.log(e);
-  }
+  };
 
-  selectMovie = (e) => {
-    const { selectedMovie } = this.state
-    const { moviePairs } = this.props
-    let selection =""
-    selection = moviePairs[e.target.getAttribute('id')]
-    this.setState({selectedMovie:selection});
-    this.props.choiceSubmitted(selection)
-  }
+  selectMovie = e => {
+    const { selectedMovie } = this.state;
+    const { moviePairs } = this.props;
+    let selection = "";
+    selection = moviePairs[e.target.getAttribute("id")];
+    this.setState({ selectedMovie: selection });
+    this.props.choiceSubmitted(selection);
+  };
   // defaultMovieImg(){
   //   const { selectedMovie } = this.state
   //   if (selectMovie.poster_path) == null
@@ -39,49 +51,47 @@ class CardBuilder extends Component {
   }
 
   render() {
-        const { dropdownOpen, selectedMovie } = this.state
-        const { moviePairs } = this.props
-        return(
-          <div className = "choice-card">
-              <Card>
-                <CardImage selectedMovie={selectedMovie} />
-                <CardBody>
-                  {selectedMovie &&
-                    <div>
-                      <CardTitle>{selectedMovie.title}</CardTitle>
-                      <CardSubtitle>{selectedMovie.genre}</CardSubtitle>
-                      <CardText>{selectedMovie.description}</CardText>
-                    </div>
-                  }
-                  {!selectedMovie &&
-                    <Dropdown isOpen={dropdownOpen} toggle={this.toggle}>
-                      <DropdownToggle caret>
-                        Movie Choices
-                      </DropdownToggle>
-                      <DropdownMenu>
-                        <DropdownItem header>Movies</DropdownItem>
-                        {moviePairs.map((movie, index) => {
-                          return(
-                            <DropdownItem key={index} id={index}
-                              onClick={this.selectMovie}>
-                              {movie.title}
-                            </DropdownItem>
-                          )
-                        })}
-                        </DropdownMenu>
-                    </Dropdown>
-                  }
-                  {selectedMovie &&
-                    <button onClick={this.cancelSelection}>
-                    Edit Choices
-                    </button>
-                  }
-
-                </CardBody>
-              </Card>
-          </div>
-        )
+    const { dropdownOpen, selectedMovie } = this.state;
+    const { moviePairs } = this.props;
+    return (
+      <div className="choice-card">
+        <Card>
+          <CardImage selectedMovie={selectedMovie} />
+          <CardBody>
+            {selectedMovie && (
+              <div>
+                <CardTitle />
+                <CardSubtitle>{selectedMovie.genre}</CardSubtitle>
+                <CardText>{selectedMovie.description}</CardText>
+              </div>
+            )}
+            {!selectedMovie && (
+              <Dropdown isOpen={dropdownOpen} toggle={this.toggle}>
+                <DropdownToggle caret>Movie Choices</DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>Movies</DropdownItem>
+                  {moviePairs.map((movie, index) => {
+                    return (
+                      <DropdownItem
+                        key={index}
+                        id={index}
+                        onClick={this.selectMovie}
+                      >
+                        {movie.title}
+                      </DropdownItem>
+                    );
+                  })}
+                </DropdownMenu>
+              </Dropdown>
+            )}
+            {selectedMovie && (
+              <button onClick={this.cancelSelection}>Edit Choices</button>
+            )}
+          </CardBody>
+        </Card>
+      </div>
+    );
   }
 }
 
-export default CardBuilder
+export default CardBuilder;
